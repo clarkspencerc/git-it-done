098dafa8aae255f5369f3169a139f999da2c2c96
clarkspencerc <clarkspencerc@gmail.com>
2022-05-16 08:47:23 -0600

    made issue list dynamic and flow from previous page

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,13 @@
 var issueContainerEl = document.querySelector("#issues-container"); 
+var limitWarningEl = document.querySelector("#limit-warning"); 
+var reponNameEl = document.querySelector("#repo-name"); 
+
+var getRepoName = function(){
+    var queryString = document.location.search; 
+    var repoName = queryString.split("=")[1];  
+    getRepoIssues(repoName); 
+    reponNameEl.textContent = repoName; 
+};
 
 var getRepoIssues = function(repo){
     console.log(repo); 
@@ -10,6 +19,11 @@ var getRepoIssues = function(repo){
             response.json().then(function(data){
                 // pass response data to dom function 
                 displayIssues(data); 
+
+                // check if api has paginated issues 
+                if(response.headers.get(displayWarning(repo))){
+                    console.log("repo has more than 30 issues")
+                }
             });
         }
         else {
@@ -56,4 +70,17 @@ var displayIssues = function(issues){
     } 
 }; 
 
-getRepoIssues("clarkspencerc/coding-quiz"); 
+var displayWarning = function(repo) {
+    // add text to warning container 
+    limitWarningEl.textContent = "To see more than 30 issues, visit"; 
+    
+    var linkEl = document.createElement("a"); 
+    linkEl.textContent = "See More Issues on Github.com"; 
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues"); 
+    linkEl.setAttribute("target", "_blank"); 
+
+    // append to warning container 
+    limitWarningEl.appendChild(linkEl); 
+
+}; 
+getRepoName(); 
